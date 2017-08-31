b8beb9adaf0d510e80072c53a0b484290f90a47e
swaggerloaf <swaggerloaf@gmail.com>
2017-08-30 23:04:27 -0400

    Updates

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloMessage from './app';
-
 // here we bootstrap our applica
 ReactDOM.render(React.createElement(HelloMessage, { name: 'Glenn' }), document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
+// Webpack uses `publicPath` to determine where the app is being served from.
+// In development, we always serve from the root. This makes config easier.
+const publicPath = '/';
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+const publicUrl = '';
+
+
 module.exports = {
     // starting point
     entry: "./app/index.js",
@@ -12,21 +21,23 @@ module.exports = {
         publicPath:'/dist'
     },
     module: {
-        loaders: [
-            {
-                test: /\.html$/,
-                loader: 'ngtemplate?relativeTo=' + (path.resolve(__dirname)) + '/app/!html'
-            },
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015'],
-                    cacheDirectory: true
-                }
-            }
-        ]
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: require.resolve('babel-loader'),
+          options: {
+            // @remove-on-eject-begin
+            babelrc: false,
+            //presets: [require.resolve('babel-preset-react-app')],
+            // @remove-on-eject-end
+            // This is a feature of `babel-loader` for webpack (not Babel itself).
+            // It enables caching results in ./node_modules/.cache/babel-loader/
+            // directory for faster rebuilds.
+            cacheDirectory: true,
+          }
+        }
+      ]
     },
     context: __dirname,
     plugins: [
