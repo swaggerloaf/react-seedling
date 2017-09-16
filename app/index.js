9a67dfec3c42c1a8d5fc3a91699fcc6af4c0af0d
swaggerloaf <swaggerloaf@gmail.com>
2017-09-15 23:18:54 -0400

    Updates

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
+import Clock from './components/clock.jsx';
+
+ReactDOM.render(React.createElement(Clock, { name: "quick" }),
+ document.getElementById('bottom')
+ );
+
 
 ReactDOM.render(React.createElement(App, { name: "quick" }),
  document.getElementById('root')
