e842b5491fadba905005719254da2a826fe0f383
swaggerloaf <swaggerloaf@gmail.com>
2017-09-15 22:49:09 -0400

    Updates

---
+++ app/components/app.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+import FaAutomobile from 'react-icons/lib/fa/automobile'
+
+export default class App extends Component {
+  render() {
+    return (
+      <h3> Lets go for a {this.props.name} <FaAutomobile /> </h3>
+    )
+  }
+}
