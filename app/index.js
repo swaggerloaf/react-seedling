02c52c92e56bdf96cb3d7fb4c075d5cd517ff0b4
swaggerloaf <swaggerloaf@gmail.com>
2018-02-09 00:24:04 -0500

    Updates

--- app/index.js
+++ app/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './styles.css';
-import App from './components/app.jsx';
-/*eslint-disable import/default */
-//import 'babel-polyfill';
-//import configureStore from './store/configureStore';;
+import Game from './components/app.jsx';
+
 import {Provider} from 'react-redux'
-//import {loadCourses} from './actions/courseActions';
-//import {loadAuthors} from './actions/authorActions';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
-//const store = configureStore();
-//store.dispatch(loadCourses());
-//store.dispatch(loadAuthors());
-ReactDOM.render(React.createElement(App),
- document.getElementById('root')
- );
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
