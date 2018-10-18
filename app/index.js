73177b3e1706b665bbb64a60018fdd99491aff2c
swaggerloaf <swaggerloaf@gmail.com>
2018-10-18 18:33:57 -0400

    Updates

--- app/index.js
+++ app/index.js
@@ -1,3 +1,7 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
