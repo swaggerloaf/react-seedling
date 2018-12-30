fc96f4a0276352445dadcf072b462ad908cc52fc
swaggerloaf <swaggerloaf@gmail.com>
2018-12-30 12:17:39 -0500

    Updates

--- app/index.js
+++ app/index.js
@@ -3,11 +3,16 @@ import * as React from 'react';
 import { render } from 'react-dom';
 //import { Provider } from 'react-redux';
 import * as styles from './styles';
-import App from './components/App.jsx';
+//import App from './components/App.jsx';
 //import store from './configStore.js';
 
+class HelloMessage extends React.Component {
+  render() {
+    return <div className="slds-text-color_success">Hi {this.props.name}</div>;
+  }
+}
 
 render(
-  <App />,
+  <HelloMessage name="Fred"></HelloMessage>,
   document.getElementById('mountNode')
 );
