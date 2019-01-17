5a4a94927dac8205abfeba1260a55209a0b64749
swaggerloaf <swaggerloaf@gmail.com>
2019-01-17 16:26:32 -0500

    Updates

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Spinner from 'react-svg-spinner';
 
 export default class Nav extends Component {
   render() {
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todos</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Spinner size="32px" color="fuchsia" />
+            </li>
+          </ul>
+        </nav>
+      </>
     );
   }
 }
