b770229dd6866c31d203306a0437c0a38b41a79b
swaggerloaf <swaggerloaf@gmail.com>
2019-01-23 16:54:12 -0500

    Updates

--- app/redux/configureStore.js
+++ app/redux/configureStore.js
@@ -1,20 +1,18 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index.js';
-import thunk from 'redux-thunk';
-//import { helloSaga } from './sagas/sagas';
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from './sagas/sagas';
 import { createLogger } from 'redux-logger';
-//const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middleware = [thunk];
-middleware.push(createLogger());
 
 const store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(middleware))
+  composeEnhancer(applyMiddleware(sagaMiddleware))
 );
 
-//sagaMiddleware.run(helloSaga);
+sagaMiddleware.run(helloSaga);
 
 //const store = createStore(rootReducer);
 export default store;
